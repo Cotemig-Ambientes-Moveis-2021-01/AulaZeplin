6b12063593c36f51d97aa1798eb08ce2f465dcd1
Dirceu Belém <dirceu@fourtime.com>
2021-03-11 20:30:10 -0300

    Implementando Tela de Profile

--- app/src/main/java/com/fingermidia/aulazeplin/MainActivity.kt
+++ app/src/main/java/com/fingermidia/aulazeplin/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.fingermidia.aulazeplin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var signup = findViewById<Button>(R.id.signup)
+        signup.setOnClickListener {
+
+            var intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
     }
 }
